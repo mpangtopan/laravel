af66f7fb0c2a9fb4ebcf6b531c9fef784fc8cfbe
KONOHA\NARUTO <topanmpang@gmail.com>
2025-05-02 10:05:44 +0700

    no message

--- resources/views/cashadvancechart.blade.php
+++ resources/views/cashadvancechart.blade.php
@@ -6,7 +6,7 @@
         <div id="bg-swap" class="card mb-4">
           <div class="card-body p-2 m-2">
               <div class="greeting">
-                <h4 style="font-style:italic">
+                <h4>
                   Cash Advance Dashboard
                 </h4>
               </div>
